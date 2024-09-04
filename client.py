cd71c3bf38e5066d06166586c8edf5cd96002e24
Nathan Kittichaikoonkij <Natsg90@gmail.com>
2024-09-04 20:35:39 +0700

    feat: client game loop

--- client.py
+++ client.py
@@ -4,8 +4,14 @@ from network import Network
 
 class Battleship:
     def __init__(self) -> None:
-        self.client = Network()
+        self.client = Network() # game state init to server
+
+
+    def start(self):
+        while True:
+            reply = self.client.send({"type": "game", "pos": input("Enter a position: ")})
+            print(reply)
 
 
 if __name__ == "__main__":
-    Battleship()
+    Battleship().start()
